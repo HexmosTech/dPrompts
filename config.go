f34e642505a4a28204bbebc56ed4a46209a32b13
RijulTP <rijulrajtkeey2@gmail.com>
2025-12-12 22:02:41 +0530

    Removing unwanted changes

--- config.go
+++ config.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/jackc/pgx/v5/pgxpool"
-	// "github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/log"
 )
 
 
@@ -25,53 +25,44 @@ func LoadDBConfig(path string) (*DBConfig, error) {
 
 
 func NewDBPool(ctx context.Context, configPath string) (*pgxpool.Pool, error) {
+
 	if configPath == "" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
-			return nil, fmt.Errorf("unable to determine home directory for config file: %w", err)
+			log.Fatal().Err(err).Msg("Unable to determine home directory for config file")
 		}
 		configPath = homeDir + string(os.PathSeparator) + ".dprompts.toml"
 	}
-
 	dbConf, err := LoadDBConfig(configPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load config: %w", err)
 	}
 
-	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		dbConf.User, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Name,
-	)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbConf.User, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Name)
 
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse DSN: %w", err)
 	}
 
-	// Set pool settings
+	// Set connection pool settings
 	config.MaxConns = 10
 	config.MinConns = 2
 	config.MaxConnLifetime = time.Hour
 	config.MaxConnIdleTime = 30 * time.Minute
 
-	// Create the pool
+	// Create the connection pool
 	dbPool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create connection pool: %w", err)
 	}
 
-	// Immediately test connection with a simple query
-	var one int
-	err = dbPool.QueryRow(ctx, "SELECT 1").Scan(&one)
-	if err != nil {
+	// Test the connection
+	if err := dbPool.Ping(ctx); err != nil {
 		dbPool.Close()
-		return nil, fmt.Errorf("unable to connect to database or DB does not exist: %w", err)
-	}
-	if one != 1 {
-		dbPool.Close()
-		return nil, fmt.Errorf("unexpected response from database")
+		return nil, fmt.Errorf("unable to ping database: %w", err)
 	}
 
 	return dbPool, nil
 }
-
